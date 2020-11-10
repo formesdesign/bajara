36233193be1d013d65b3b428295e7d19f8896058
formesdesign <72689061+formesdesign@users.noreply.github.com>
2020-11-11 00:32:02 +0100

    segunda versio

--- baraja.py
+++ baraja.py
@@ -2,18 +2,14 @@
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S","C", "R"]
 
-baraja = []  #llista vuida
 #recorrem les llistes en un for: palo i numero son items de les cadenes x fer la conbinació de les dos llistes
 
 def creaBaraja(): #definim funcion
+    baraja = []  #variable local vuida
     for palo in palos:
         for numero in numeros:
             baraja.append(numero+palo) #iteración
-
     return baraja
-
-creaBaraja() #aci ejecutem el def
-print(baraja)
 
 
 
