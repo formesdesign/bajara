f12692954fc58fed241ad0839933f75d6cf6dfdb
formesdesign <72689061+formesdesign@users.noreply.github.com>
2020-11-12 22:10:00 +0100

    metodo repartir a classe baraj

--- barajaClasse.py
+++ barajaClasse.py
@@ -9,3 +9,14 @@ class Baraja():
 
     def barajar(self):
         baraja.barajar(self.mazacote)
+
+    def repartir(self, numjugadores, numcartas):
+        jugadores = [] #llists buida
+        for i in range(numjugadores):
+            jugadores.append([])
+
+        for carta in range(numcartas):
+            for jugador in range(numjugadores):
+                jugadores[jugador].append(self.mazacote.pop(0)) #añade lo que estas sacando en la lista jugador
+
+        return jugadores
--- main.py
+++ main.py
@@ -6,15 +6,24 @@ ordenada = baraja.creaBaraja()
 print("Esta es la 1 baraja" ,ordenada)
 
 desordenada = baraja.creaBaraja()
-print ("esta es la 2 només crarla", desordenada)
+print ("esta es la 2 només crear-la", desordenada)
 baraja.barajar(desordenada)
 print("i ahora desordenada", desordenada)
 
 '''
+import barajaClasse
+
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S","C", "R"]
 
-import barajaClasse
-
 miBaraja = barajaClasse.Baraja(palos, numeros)
+
+miBaraja.mazacote[2]
+
 print(miBaraja.mazacote)
+
+print(miBaraja.repartir(3,5))
+
+miBaraja.barajar()
+print(miBaraja.mazacote)
+
